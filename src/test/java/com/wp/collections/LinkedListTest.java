901d7fb021df0e723ce03e613b4d6606b1d37bbc
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-11-03 14:25:13 +0100

    added MyLinkedList tests

---
+++ src/test/java/com/wp/collections/LinkedListTest.java
@@ -0,0 +1,10 @@
+package com.wp.collections;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListTest extends ListTest{
+    public LinkedListTest() {
+        super(new LinkedList<>());
+    }
+}
---
+++ src/test/java/com/wp/collections/MyLinkedListTest.java
@@ -0,0 +1,9 @@
+package com.wp.collections;
+
+import java.util.List;
+
+public class MyLinkedListTest extends ListTest{
+    public MyLinkedListTest() {
+        super(new MyLinkedList<>());
+    }
+}
