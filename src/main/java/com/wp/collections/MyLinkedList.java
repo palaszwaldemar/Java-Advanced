3ed58987e9decfcc2627350041bbea6bdc1a2a7f
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-11-21 12:22:40 +0100

    added "MyLinkedList" class, and override methods internal methods.

--- src/main/java/com/wp/collections/MyLinkedList.java
+++ src/main/java/com/wp/collections/MyLinkedList.java
@@ -2,32 +2,56 @@ package com.wp.collections;
 
 import java.util.*;
 
+// CHECK : 19.11.2022 jak działa debugger?
 public class MyLinkedList<T> implements List<T> {
-    private MyNode<T> head;
+    private MyNode<T> head;//firstNode
 
-    @Override
+    @Override // CHECK : 09.11.2022
     public int size() {
-        return 0;
+        int count = 0;
+        MyNode<T> actualNode = head;
+        while (actualNode.isNotLast()) {
+            actualNode = actualNode.getNextMyNode();
+            count++;
+        }
+        count++;
+        return count;
     }
 
-    @Override
+    @Override // CHECK : 09.11.2022
     public boolean isEmpty() {
-        return false;
+        return head == null;
     }
 
-    @Override
+    @Override // CHECK : 18.11.2022
     public boolean contains(Object o) {
-        return false;
+        MyNode<T> actualNode = head;
+        while (actualNode.isNotLast()) {
+            if (actualNode.getObject().equals(o)) {
+                return true;
+            }
+            actualNode = actualNode.getNextMyNode();
+        }
+        return actualNode.getObject().equals(o);
     }
 
-    @Override
+    @Override // TODO: 19.11.2022
     public Iterator<T> iterator() {
         return null;
     }
 
-    @Override
+    @Override // CHECK : 17.11.2022
     public Object[] toArray() {
-        return new Object[0];
+        Object[] array = new Object[size()];
+        int indexOfArray = 0;
+        MyNode<T> actualNode = head;
+        array[indexOfArray++] = actualNode.getObject();
+        while (actualNode.isNotLast()) {
+            actualNode = actualNode.getNextMyNode();
+            array[indexOfArray] = actualNode.getObject();
+            indexOfArray++;
+        }
+        return array;
     }
 
     @Override
@@ -35,27 +59,33 @@ public class MyLinkedList<T> implements List<T> {
         return null;
     }
 
-    @Override
+    @Override // CHECK : 17.11.2022
     public boolean add(T t) {
-        MyNode<T> myNode = head;
-        do {
-            myNode = myNode.getNextMyNode();
-        } while (!myNode.isLast());
-        myNode.setNextMyNode(new MyNode<>(t));
+        if (head == null) {
+            head = new MyNode<>(t);
+            return true;
+        }
+        MyNode<T> actualNode = head;
+        while (actualNode.isNotLast()) {
+            actualNode = actualNode.getNextMyNode();
+        }
+        actualNode.setNextMyNode(new MyNode<>(t));
         return true;
     }
 
-    @Override
+    @Override // CHECK : 17.11.2022
     public T get(int index) {
-        if (index == 0) {
-            return head.getObject();
+        MyNode<T> actualNode = head;
+        for (int i = 0; i < index; i++) {
+            actualNode = actualNode.getNextMyNode();
         }
-        return head.getNextMyNode().getObject();
+        return actualNode.getObject();
     }
 
-    @Override
+    @Override // CHECK : 19.11.2022
     public boolean remove(Object o) {
-        return false;
+        remove(indexOf(o));
+        return true;
     }
 
     @Override
@@ -83,29 +113,81 @@ public class MyLinkedList<T> implements List<T> {
         return false;
     }
 
-    @Override
+    @Override // CHECK : 17.11.2022
     public void clear() {
-
+        head = null;
     }
 
-    @Override
+    @Override // TODO: 18.11.2022 dokończyć
     public T set(int index, T element) {
         return null;
     }
 
-    @Override
+    @Override // TODO: 19.11.2022
     public void add(int index, T element) {
-
+        MyNode<T> myNode = head;
+        MyNode<T> myNodeToAdd = new MyNode<>(element);
+        int count = 0;
+        while (myNode.isNotLast()) {
+            if (count == index - 1) {
+                MyNode<T> aditionalMyNode = myNode.getNextMyNode();
+                myNode.setNextMyNode(myNodeToAdd);
+                myNodeToAdd.setNextMyNode(aditionalMyNode);
+                return;
+            } else {
+                myNode = myNode.getNextMyNode();
+            }
+            count++;
+        }
+        // TODO: 20.11.2022 dodać instrukcje w przypadku gdy myNode jest ostatnie lub lista jest jedno elementowa
     }
 
-    @Override
+    @Override // CHECK : 19.11.2022
     public T remove(int index) {
-        return null;
+        MyNode<T> myNode = head;
+        int count = 0;
+        MyNode<T> myNodeToDelete = myNode;
+        while (myNode.isNotLast()) {
+            if (count == index - 1) {
+                myNodeToDelete = myNode.getNextMyNode();
+                MyNode<T> secondMyNode = myNode.getNextMyNode();
+                myNode.setNextMyNode(secondMyNode);
+            }
+            myNode = myNode.getNextMyNode();
+            count++;
+        }
+        /*if (index == 0) {
+            head = myNode.getNextMyNode();
+            return myNode.getObject();
+        }
+        for (int i = 0; i < index - 1; i++) {//pętla, która iteruje do elementu poprzedzającego element do usunięcia
+            myNode = myNode.getNextMyNode();
+        }
+        MyNode<T> deletedMyNode = myNode.getNextMyNode();
+        MyNode<T> secondMyNode = myNode;
+        for (int i = 0; i < 2; i++) {//pętla, ktora iteruje (od elementu poprzedzającego element do usunięcia) do
+            //elementu nastęnego po elemencie do usunięcia
+            secondMyNode = secondMyNode.getNextMyNode();
+        }
+        myNode.setNextMyNode(secondMyNode);*/
+        return myNodeToDelete.getObject();
     }
 
-    @Override
+    @Override // CHECK : 19.11.2022
     public int indexOf(Object o) {
-        return 0;
+        int index = 0;
+        MyNode<T> myNode = head;
+        while (myNode.isNotLast()) {
+            if (myNode.getObject().equals(o)) {
+                return index;
+            }
+            myNode = myNode.getNextMyNode();
+            index++;
+        }
+        if (myNode.getObject().equals(o)) {
+            return index;
+        }
+        return -1;
     }
 
     @Override
